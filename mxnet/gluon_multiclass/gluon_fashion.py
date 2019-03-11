2544a06cb73ff9ce4ac78976c7829ccfaa27886a
Lyn <james_gosling@163.com>
2019-03-11 22:14:15 +0800

    ===修复sgd错误===== 使用gluon实现多分类  2019-03-11 22:13:59

---
+++ mxnet/gluon_multiclass/gluon_fashion.py
@@ -0,0 +1,16 @@
+from mxnet import gluon as gl
+from mxnet import ndarray as nd
+
+
+def transform(data, label):
+    return data.astype('float32') / 255, label.astype('float32')
+
+def show_imgs():
+
+
+if __name__ == '__main__':
+    mnist_train = gl.data.vision.FashionMNIST(train=True, transform=transform)
+    mnist_test = gl.data.vision.FashionMNIST(train=False, transform=transform)
+
+    data, label = mnist_train[0]
+    print('example data : %s , label : %s ' % (data.shape, label))
